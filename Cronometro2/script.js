3b08aff40f194ab26bd6e3805d10459a93416bdb
thiagomezacasa <tsmezacasa@gmail.com>
2023-05-25 16:52:32 -0300

    ok

--- Cronometro2/script.js
+++ Cronometro2/script.js
@@ -2,52 +2,67 @@ const timer = document.querySelector("#timer");
 
 const iniciar = document.getElementById("start");
 const pausar = document.getElementById("pause");
+const zerar = document.getElementById("stop");
+
+let pausou  = false;
+
 var cronometro;
 var formatado;
 
 iniciar.addEventListener("click", () => {
-    const tmpini = Date.now();
+    if (!pausou){
+        const tmpini = Date.now();
 
-    const contador = ()=> {
+        const contador = ()=> {
         
-        const tmpatual = Date.now();
-        let cont = tmpatual - tmpini;
-    
-        let seg = (tmpatual-tmpini)/1000;
-        console.log(seg)
-        timer.innerHTML = converter(seg);
-    
-    }
-    
-    const converter = (seg)=>{
-        const hora = Math.floor(seg/3600);
-        const resto = seg%3600;
-       
-        const min = Math.floor(resto/60);
-    
-        const segundo = Math.floor(resto%60); 
-    
-        formatado = (hora<10?"0"+hora:hora)+":"+(min<10?"0"+min:min)+":"+(segundo<10?"0"+segundo:segundo)
-        return formatado;
-    }
+            const tmpatual = Date.now();
+            let cont = tmpatual - tmpini;
+        
+            let seg = (tmpatual-tmpini)/1000;
+        // console.log(seg)
+            timer.innerHTML = converter(seg);
+        
+        }
+        
+        const converter = (seg)=>{
+            const hora = Math.floor(seg/3600);
+            const resto = seg%3600;
+        
+            const min = Math.floor(resto/60);
+        
+            const segundo = Math.floor(resto%60); 
+        
+            formatado = (hora<10?"0"+hora:hora)+":"+(min<10?"0"+min:min)+":"+(segundo<10?"0"+segundo:segundo)
+            return formatado;
+        }
 
-    cronometro = setInterval(contador,1000);
-    pausar.innerText = "Pausar";
+        cronometro = setInterval(contador,1000);
+        
+    }    
 })
 
 function stopPause(e) {
     clearInterval(cronometro);
-    //e.stopPropagation();
+    pausou = true;
 }
 
 pausar.addEventListener("click", () => {
     stopPause();
+    /*let label_tmp = pausar.textContent;
+    if (cronometro){
+        if (label_tmp == 'Pausar'){
+            console.log(formatado)
+            pausar.textContent = 'Prosseguir'
     
-    if (pausar.textContent = 'Pausar'){
-        pausar.innerText = 'Prosseguir'
+        }
 
-    }
-    console.log(formatado)
-    
-    
+    }else{
+        alert("Primeiro iniciar")
+    }*/
+})
+
+zerar.addEventListener("click", () =>{
+    timer.innerHTML = "00:00:00"
+
+
 })
